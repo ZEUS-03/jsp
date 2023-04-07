cfb03c798e67b823c05a93aa7f302f7eb4dd42d9
Gautam Sharma <gautam332000lavanya@gmail.com>
2023-04-07 17:05:55 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const email = document.getElementById("email");
 MINIMUM_PASS_LENGTH = 8;
 
 const username = document.getElementById("username");
-let flag = true;
 
 const isValidEmail = (email) => {
     const re =
@@ -15,9 +14,15 @@ const isValidEmail = (email) => {
     return re.test(String(email.toLowerCase()));
 };
 
-// form.addEventListener("submit", (e) => {
-//     validateInputs();
-// });
+form.addEventListener("submit", (e) => {
+    let flag = true;
+    validateInputs();
+    if (flag == false) {
+        return false;
+    } else {
+        return true;
+    }
+});
 
 const setSuccess = (element) => {
     const inputControl = element.parentElement;
@@ -33,7 +38,7 @@ const setError = (element, message) => {
     errorDisplay.innerText = message;
     inputControl.classList.add("error");
     inputControl.classList.remove("success");
-    preventDefault();
+    flag = false;
 };
 
 function validateInputs() {
@@ -42,54 +47,36 @@ function validateInputs() {
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
     const emailValue = email.value.trim();
-    let flag = false;
-    let error = 0;
-    console.log(error);
 
     if (userValue === "") {
         setError(user, "enter username!");
-        error++;
     } else {
         setSuccess(user);
     }
     if (lnameValue === "") {
         setError(lname, "enter last name!");
-        error++;
     } else {
         setSuccess(lname);
     }
     if (emailValue === "") {
         setError(email, "enter email!");
-        error++;
     } else if (!isValidEmail(emailValue)) {
         setError(email, "Please enter a valid email.");
-        error++;
     } else {
         setSuccess(email);
     }
     if (passwordValue === "") {
         setError(password, "enter Password!");
-        error++;
     } else if (passwordValue.length < MINIMUM_PASS_LENGTH) {
         setError(password, "Password length is too short");
-        error++;
     } else {
         setSuccess(password);
     }
     if (password2Value === "") {
         setError(password2, "reenter password!");
-        error++;
     } else if (password2Value !== passwordValue) {
         setError(password2, "Password doesn't match!");
-        error++;
     } else {
         setSuccess(password2);
     }
-    if (error === 0) {
-        flag = true;
-    } else {
-        flag = false;
-    }
-
-    return flag;
 }
